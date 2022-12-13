dc5ac7e048b968fd524eb612173c80f4db372bae
Legendarik2004 <alvaro.rodriguez.gordon.2004@gmail.com>
2022-12-13 23:08:32 +0100

    Commit2

--- Prueba.java
+++ Prueba.java
@@ -1,9 +1,10 @@
-public class das {
+public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
     System.out.print("HOLA MUNDO");
     System.out.print("Mi primer commit");
+    System.out.print("Mi segundo commit");
 	}
 }
